76a68f66d928c529b4e942b93f5f7ccd3b273958
Christopher Tabone <christabone@gmail.com>
2019-10-08 13:48:35 -0300

    First pass at adding IMEX data to ferret.

--- src/compression/compression.py
+++ src/compression/compression.py
@@ -8,6 +8,12 @@ def gunzip_file(**kwargs):
     os.system("gunzip -f {}/{}".format(kwargs['savepath'], kwargs['filename']))
 
 
+def unzip_file(**kwargs):
+    logger.info('{}: Extracting file with unzip: {}'.format(kwargs['worker'], kwargs['filename']))
+    os.s
+    ystem("unzip {}/{}".format(kwargs['savepath'], kwargs['filename']))
+
+
 def no_compression(**kwargs):
     logger.info('{}: File suffix \'{}\' not found in decompression dictionary.'.format(kwargs['worker'], kwargs['file_suffix']))
     logger.info('{}: Skipping decompression for {}.'.format(kwargs['worker'], kwargs['filename']))
@@ -18,7 +24,8 @@ def no_compression(**kwargs):
 def decompress(worker, filename, savepath):
     # After writing functions for decompression, please add them to this dispatch table by suffix.
     dispatch_dict = {
-        'gz': gunzip_file
+        'gz': gunzip_file,
+        'zip': unzip_file
     }
 
     file_suffix = filename.split('.')[-1]
